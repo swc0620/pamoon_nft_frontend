4881b5c829d5221ac27fc860529e8c566d8f09cf
swc0620 <pinetreewave@naver.com>
2022-07-05 17:58:51 +0900

    changed image url fetching method

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Wrap=styled.div`
-  background-color:white;
-  margin:20px 10px;
-  border:1px solid #DBDBDB;
-  border-radius:10px;
-  width:300px;
-  height:270px;
+const Wrap = styled.div`
+  background-color: white;
+  margin: 20px 10px;
+  border: 1px solid #DBDBDB;
+  border-radius: 10px;
+  width: 300px;
+  height: 270px;
   box-shadow: 0px 0px 21px 3px rgba(0, 0, 0, 0.25);
-  img{
-    width:300px;
-    height:140px;
-    border-radius:10px 10px 0px 0px;
+  img {
+    width: 300px;
+    height: 140px;
+    border-radius: 10px 10px 0px 0px;
   }
-  .text{
-    text-align:center;
-    margin-top:30px;
-    .token-number{
-      font-size:20px;
-      font-weight:700;
-      padding:5px;
+  .text {
+    text-align: center;
+    margin-top: 30px;
+    .token-number {
+      font-size: 20px;
+      font-weight: 700;
+      padding: 5px;
     }
-    .wallet-address{
-      font-size:12px;
-      color:#6A6A6A;
+    .wallet-address {
+      font-size: 12px;
+      color: #6A6A6A;
     }
   }
 `;
 
 function Card(props) {
-  const img=props.img;
-  const wallet=props.wallet;
-  const number=props.number;
+  const img = props.img;
+  const wallet = props.wallet;
+  const number = props.number;
+  
   return (
     <Wrap>
       <img alt='img' src={img}></img>
@@ -44,4 +45,4 @@ function Card(props) {
   )
 }
 
-export default Card
+export default Card;
