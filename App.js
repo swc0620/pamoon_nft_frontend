28e38dc2796006d34c7486fdf43c99a640136570
snu_mech_18 <97350083+mi-gongan@users.noreply.github.com>
2022-07-06 13:24:51 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Logo from './components/Logo';
+import Template from './components/Template';
+
+
+
+function App() {
+
+  return (
+    <>
+      <Logo img="Pamoon"></Logo>
+      <Template></Template>
+    </>
+  );
+}
+
+export default App;
