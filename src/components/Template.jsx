6a52a85af8259bddbda4be3c13344af7afed8019
swc0620 <pinetreewave@naver.com>
2022-07-07 20:53:53 +0900

    removing debuggings

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -22,22 +22,17 @@ function Template() {
     const fetchData = async () => {
       const cardArray = [];
       try {
-        console.log(1);
         const caver = new Caver(process.env.REACT_APP_CYPRESS_URI);
         const krafterspaceContract = new caver.contract(KIP17.abi, process.env.REACT_APP_KRAFTERSPACE_CONTRACT_ADDRESS, { gasPrice: caver.utils.toBN(process.env.REACT_APP_GAS_PRICE).toString() });
         const pamoonContract = new caver.contract(PamoonNFTArtifact.abi, process.env.REACT_APP_PAMOON_NFT_CONTRACT_ADDRESS, { gasPrice: caver.utils.toBN(process.env.REACT_APP_GAS_PRICE).toString() });
-        console.log(2);
+
         // krafterspace contract tokens
         const tokens = process.env.REACT_APP_TOKEN_IDS.split(', ').map(id => parseInt(id));
-        console.log(3);
+
         while (true) {
           if (cardId.current < 5) {
             // krafterspace contract
-            console.log(tokens);
-            console.log(cardId.current);
-            console.log(typeof tokens[cardId.current]);
             const imgJSON = await krafterspaceContract.methods.tokenURI(caver.utils.toBN(tokens[cardId.current]).toString()).call();
-            console.log(4);
             let imgData;
             await fetch(imgJSON)
               .then((result) => result.json())
@@ -46,7 +41,6 @@ function Template() {
               })
               .catch((err) => console.error(err));
             
-            console.log(5);
             const walletData = await krafterspaceContract.methods.ownerOf(caver.utils.toBN(tokens[cardId.current]).toString()).call();
             const newItem = {
               id: cardId.current,
@@ -54,13 +48,10 @@ function Template() {
               wallet: walletData,
             };
             cardArray.push(newItem);
-            console.log(6);
           } else {
             // pamoon contract
             try {
-              console.log(7);
               const imgData = await pamoonContract.methods.tokenURI(cardId.current-4).call();
-              console.log(8);
               const walletData = await pamoonContract.methods.ownerOf(cardId.current-4).call();
               const newItem = {
                 id: cardId.current,
@@ -68,7 +59,6 @@ function Template() {
                 wallet: walletData,
               };
               cardArray.push(newItem);
-              console.log(9);
             } catch(e) {
               console.log(e)
               break;
